17136e7bc33bde3741362bffc72f0034db20d4c5
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-18 22:07:36 +0000

    Server JS

---
+++ server.js
@@ -0,0 +1,22 @@
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({port: 8181});
+var uuid = require('node-uuid'),
+   _     = require('lodash')._;
+
+var stocks = {
+    "AAPL": 95.0,
+    "MSFT": 50.0,
+    "AMZN": 300.0,
+    "GOOG": 550.0,
+    "YHOO": 35.0,
+    "FB": 75.0
+};
+
+var stockUpdater;
+var randomStockUpdater = function(){
+    for (var symbol in stocks){
+        if(stocks.hasOwnProperty(symbol)){
+            
+        }
+    }
+}
