066310370e646ecf49549fcc9f7dd184201ad267
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-19 22:05:31 +0000

    Server

--- server.js
+++ server.js
@@ -12,13 +12,59 @@ var stocks = {
     "FB": 75.0
 };
 
-
 var stockUpdater;
 var randomStockUpdater = function(){
     for (var symbol in stocks){
         if(stocks.hasOwnProperty(symbol)){
-            var randomized
-
+            var randomized = randomInterval(-150,150);
+            var floatChange = randomizedChange / 100;
+            stocks[symbol] += floatChange;
         }
     }
-}
+    var randomMSTime = randomInterval(500, 2500);
+    stockUpdater = setTimeout(function() {
+        randomStockUpdater();
+    }, randomMSTime);
+};
+
+
+randomStockUpdater();
+
+wss.on('connection', function(ws) {
+    var client_uuid = uuid.v4();
+    console.log('client [%s] connected', client_uuid);
+
+    var clientStockUpdater;
+    var sendStockUpdates = function(ws) {
+        if(ws.readyState == 1) {
+            var stocksObj = {};
+
+            for(var i=0; i<clientStocks.length; i++) {
+                symbol = clientStocks[i];
+                stocksObj[symbol] = stocks[symbol];
+            }
+
+            ws.send(JSON.stringify(stocksObj));
+        }
+    };
+
+    clientStockUpdater = setInterval(function() {
+        sendStockUpdates(ws);
+    }, 1000);
+
+    var clientStocks = [];
+
+    ws.on('message', function(message) {
+      var stock_request = JSON.parse(message);
+      clientStocks = stock_request['stocks'];
+      sendStockUpdates(ws);
+    });
+
+    ws.on('close', function() {
+        if(typeof clientStockUpdater !== 'undefined') {
+            clearInterval(clientStockUpdater);
+        }
+    });
+});
+
+console.log("wtf");
